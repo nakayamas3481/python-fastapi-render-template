07723c0d06c7b04084103f18ec1019ce2e61d442
nakayamas3481 <sakura3481@gmail.com>
2025-11-17 16:40:17 +0530

    Change

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ async def root():
 @app.get("/hi")
 async def hi():
   return {"Hi": "Sota"}
+
+@app.get("/add/")
+async def add(x: int = 0, y: int = 0):
+  return {"result": x+y}
+
+@app.get("/multiply/")
+async def multiply(x: int = 0, y: int = 0):
+  return {"result": x*y}
