f9fa1a535a0f3e5508900310999fc97aedb56133
nakayamas3481 <sakura3481@gmail.com>
2025-11-19 08:52:32 +0530

    任意のコミットメッセージ

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -21,3 +21,38 @@ async def add(x: int = 0, y: int = 0):
 @app.get("/multiply/")
 async def multiply(x: int = 0, y: int = 0):
   return {"result": x*y}
+
+jobBoards = {
+    "acme": [
+      {
+        "title": "Customer Support Executive"  
+      },
+      {
+        "title": "Project Manager"
+      }
+    ],
+    "bcg": [
+      {
+         "title": "Technical Arcitect"
+      },
+      {
+        "title": "Junior Software Developer"
+      }
+    ],
+    "atlas": [
+      {
+        "title": "Technical Arcitect",
+        "jobDescription":"ABC"
+      },
+      {
+        "title": "Junior Software Developer",
+        "jobDescription":"DEF"
+      }
+    ]
+}
+
+@app.get("/job-boards/{slug}")
+async def company_job_board(slug : str):
+#  if slug not in jobBoards:
+#         raise HTTPException(status_code=404, detail="Item not found")
+ return jobBoards[slug]
