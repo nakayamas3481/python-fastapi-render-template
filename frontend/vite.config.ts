4abaadcead3b1fdc17f4a15338a8cd9103b7b01e
nakayamas3481 <sakura3481@gmail.com>
2025-11-28 14:40:37 +0530

    End of DAY5 of week3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -19,12 +18,5 @@ export default defineConfig({
       }
     }
   }, 
-  // ★追加: shadcnが使う alias を Vite に教える
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "app"),
-      "@": path.resolve(__dirname, "app"),
-    },
-  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
