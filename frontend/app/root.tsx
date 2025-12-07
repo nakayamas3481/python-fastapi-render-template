e2abeb6cdf7ae8292ba9b2e649a2b89c6ea66060
nakayamas3481 <sakura3481@gmail.com>
2025-12-07 22:56:57 +0530

    lab7 and lab8 finish

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,19 +1,22 @@
-import { Outlet } from "react-router";
-import "./app.css"
+import { Outlet } from "react-router"
+import appStylesHref from "./app.css?url"
 import { authMiddleware } from "./middleware";
+import type { Route } from "./+types/root";
 
 export const clientMiddleware: Route.ClientMiddlewareFunction[] = [authMiddleware];
 
 export default function App() {
-    return (
-      <html>
-        <head>
-          <title>Jobify</title>
-        </head>  
-        <body>
-          <Outlet></Outlet>
-        </body>
-      </html>
-    );
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href={appStylesHref} />
+        <title>Jobify</title>
+      </head>
+      <body>
+        <Outlet></Outlet>
+      </body>
+    </html>
+  );
 }
-
